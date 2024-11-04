22f1e319a7489b2b5a8af765ebcdb5c6289c60c7
jinwooshin <sjw0851@gmail.com>
2024-11-05 02:43:39 +0900

    Ioc Container와 Bean

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -4,13 +4,17 @@ import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class MemoService {
 
     private final MemoRepository memoRepository;
 
+    @Autowired
     public MemoService(MemoRepository memoRepository) {
         this.memoRepository = memoRepository;
     }
